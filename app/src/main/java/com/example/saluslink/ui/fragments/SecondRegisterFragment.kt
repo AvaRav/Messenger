72bfe29f0cd157bbbd14728eea0561231c89f58f
Nuya <IrinaAlexandrova87@yandex.ru>
2023-05-08 00:29:34 +0300

    Add registration menu xml-file

---
+++ app/src/main/java/com/example/saluslink/ui/fragments/SecondRegisterFragment.kt
@@ -0,0 +1,12 @@
+package com.example.saluslink.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.saluslink.R
+
+class SecondRegisterFragment : Fragment(R.layout.fragment_second_register) {
+
+}
